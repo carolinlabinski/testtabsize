69ddb644affa8de36018cee78120f3089497cd33
carolin.labinski <carolin.labinski@yahoo.de>
2020-06-30 10:32:29 +0200

    testy

--- test/src/App.js
+++ test/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./pages/home.jsx";
-
+import CreateTeam from "./pages/createTeam.jsx";
 
 function App() {
   return (
     <Router>
     <div >
     <Route exact path="/"> <Home /> </Route>
+    <Route exact path="/create-team"> <CreateTeam /> </Route>
     </div>
     </Router>
   );
---
+++ test/src/pages/createTeam.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function CreateTeams() {
+  return (
+    <div>
+      <hr className="my-4 mt-5" style={{ width: "600px" }}></hr>
+      <div
+        className="bg-dark pb-3 p-2 mx-auto rounded select"
+        style={{ width: "35%" }}
+      >
+        <h3 className="text-light text-center">Add a new team</h3>
+      </div>
+      <hr className="my-4" style={{ width: "600px" }}></hr>
+      <h3>This is a fucking test</h3>
+    </div>
+  );
+}
+
+export default CreateTeams;
